94c4dd1bef9f6823da1f67020d9050c7f91265d8
pandavan <nathlumia@outlook.com>
2015-06-20 00:41:53 +0530

    Update Zad-dl.sh

--- Zad-dl.sh
+++ Zad-dl.sh
@@ -5,139 +5,125 @@
 
 
 function YouDown {
-	
+
 # Copying and validation of url from clipbord using xsel
-regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]';
-clpy=$(xsel -b);
-if [[ $clpy =~ $regex ]]
-then 
- clpy=$clpy;
-    
-else
-    clpy="http://"
-fi
+	regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]';
+	clpy=$(xsel -b);
+	if [[ $clpy =~ $regex ]]
+		then 
+			clpy=$clpy;
 
-OUTPUT="$(yad --width="500" --title="Zad-dl" --text="Download audio and video files" --form --field="URL" --field="Directory:MDIR" --field="Proxychains:CHK" "$clpy" "$HOME/Desktop/" FALSE --button="Fetch URL:2" --button="gtk-ok:0" --button="gtk-cancel:1")"
-  
-accepted=$?
+	else
+		clpy="http://"
+			fi
 
-if [ $accepted -eq 2 ]; then
-YouDown;
-exit
-elif [ $accepted -eq 1 ]; then
-    echo "Cancelled"
-    exit 1
-elif  [ $accepted -eq 0 ]; then
-echo ""
-else
- echo "something went wrong"
- exit 1 
-fi
+			OUTPUT="$(yad --width="500" --title="Zad-dl" --text="Download audio and video files" --form --field="URL" --field="Directory:MDIR" --field="Proxychains:CHK" "$clpy" "$HOME/Downloads/" FALSE --button="Fetch URL:2" --button="gtk-ok:0" --button="gtk-cancel:1")"
 
-URLy=$(awk -F'|' '{print $1}' <<<$OUTPUT)
-DIRE=$(awk -F '|' '{print $2}' <<<$OUTPUT)
-CHKy=$(awk -F '|' '{print $3}' <<<$OUTPUT)
+			accepted=$?
 
+			if [ $accepted -eq 2 ]; then
+				YouDown;
+	exit
+		elif [ $accepted -eq 1 ]; then
+		echo "Cancelled"
+		exit 1
+		elif  [ $accepted -eq 0 ]; then
+		echo ""
+			else
+				echo "something went wrong"
+					exit 1 
+					fi
+
+					URLy=$(awk -F'|' '{print $1}' <<<$OUTPUT)
+					DIRE=$(awk -F '|' '{print $2}' <<<$OUTPUT)
+					CHKy=$(awk -F '|' '{print $3}' <<<$OUTPUT)
 #URL validation\
-if [[ $(echo ${#URLy}) -lt 14 ]] ; then
-zenity --info --width=300 --title="Zad-dl" --timeout=3 --text="Wrong Url input"
-YouDown;
-fi
-
+					if [[ $(echo ${#URLy}) -lt 14 ]] ; then
+					zenity --info --width=300 --title="Zad-dl" --timeout=3 --text="Wrong Url input"
+					YouDown;
+	fi
 # DIrectory Location
-cd $DIRE;
-
+		cd $DIRE;
 #validating proxy option
-prox="";
-if [ "$CHKy" = "TRUE" ]; then
-prox="proxychains"
-msg="\nINFO: You are using a proxy and can cause slower download speed"
-fi
-
-
+	prox="";
+	if [ "$CHKy" = "TRUE" ]; then
+		prox="proxychains"
+			msg="\nINFO: You are using a proxy and can cause slower download speed"
+			fi
 #Selecting a format to download, added addition line 'mp3'
-
-FRMT=$($prox youtube-dl -F $URLy |grep -vE '^\[(youtube|info|download|youtube:playlist)\] |^format |^ProxyChains'| sed '1imp3' |zenity  --list --width=500 --height=400 --title="Zad-dl" --text "Fetching audio, video format... $msg" --ok-label="Start" --column "File Format"); 
-
-if [[ $? -eq 1 ]]; then
-zenity --info --width=300 --title="Zad-dl" --timeout=2 --text="Task Quit"
-exit
-elif [[ -z $FRMT ]]; then
-zenity --info --width=300 --title="Zad-dl" --timeout=3  --text="No Format Selected.\n Allow time to fetch file format details"
-exit
-else
+			FRMT=$($prox youtube-dl -F $URLy |grep -vE '^\[(youtube|info|download|youtube:playlist)\] |^format |^ProxyChains'| sed '1imp3' |zenity  --list --width=500 --height=400 --title="Zad-dl" --text "Fetching audio, video format... $msg" --ok-label="Start" --column "File Format"); 
+	if [[ $? -eq 1 ]]; then
+		zenity --info --width=300 --title="Zad-dl" --timeout=2 --text="Task Quit"
+			exit
+			elif [[ -z $FRMT ]]; then
+			zenity --info --width=300 --title="Zad-dl" --timeout=3  --text="No Format Selected.\n Allow time to fetch file format details"
+			exit
+	else
 #Selecting ID number from FRMT
-strg="$(awk '{print $1}' <<<"$FRMT")"; 
+		strg="$(awk '{print $1}' <<<"$FRMT")"; 
 #Download files using youtube-dl
-if [ "$strg" = "mp3" ]; then
+	if [ "$strg" = "mp3" ]; then
 #Download Mp3 audios and Playlist
-$prox youtube-dl --extract-audio --audio-quality 0 --newline --audio-format mp3 \
-           $URLy |
- grep --line-buffered -oP '^\[download\].*?\K([0-9.]+\%|#\d+ of \d)' | 
-    zenity --progress \
-  --title="Zad-dl" \
-   --width="350" \
-  --text="Directory Path: $DIRE \nDownloading mp3, Please Wait..." \
-  --ok-label="Directory" \
-  --percentage=0 ;  
- if [[ $? -eq 0 ]]; then
- #Open directory in a GUI
- xdg-open $DIRE;
- sleep 2
-
-fi
-
-elif [[ $strg != "mp3" ]];then
+		$prox youtube-dl --extract-audio --audio-quality 0 --newline --audio-format mp3 \
+			$URLy |
+			grep --line-buffered -oP '^\[download\].*?\K([0-9.]+\%|#\d+ of \d)' | 
+			zenity --progress \
+			--title="Zad-dl" \
+			--width="350" \
+			--text="Directory Path: $DIRE \nDownloading mp3, Please Wait..." \
+			--ok-label="Directory" \
+			--percentage=0 ;  
+	if [[ $? -eq 0 ]]; then
+#Open directory in a GUI
+		xdg-open $DIRE;
+	sleep 2
+		fi
+		elif [[ $strg != "mp3" ]];then
 #Download Other types of files and Playlist
-$prox youtube-dl --newline -f $strg $URLy\
-           $URLy |
- grep --line-buffered -oP '^\[download\].*?\K([0-9.]+\%|#\d+ of \d)' | 
-    zenity --progress \
-  --title="Zad-dl" \
-   --width="350" \
-  --text="Directory Path: $DIRE \nDownloading, Please Wait..." \
-  --ok-label="Directory" \
-  --percentage=0 ;  
-if [[ $? -eq 0 ]]; then
- xdg-open $DIRE;
- sleep 2
-fi
-fi
-fi
+		$prox youtube-dl --newline -f $strg $URLy\
+		$URLy |
+		grep --line-buffered -oP '^\[download\].*?\K([0-9.]+\%|#\d+ of \d)' | 
+		zenity --progress \
+		--title="Zad-dl" \
+		--width="350" \
+		--text="Directory Path: $DIRE \nDownloading, Please Wait..." \
+		--ok-label="Directory" \
+		--percentage=0 ;  
+	if [[ $? -eq 0 ]]; then
+		xdg-open $DIRE;
+	sleep 2
+		fi
+		fi
+		fi
 }
-
 echo -e "Welcome to Zad-dl...A DIY downloader for Ubuntu distros\n"
-
-
 #checking necessary programms installed or not
 if [ ! -x "$(command -v youtube-dl)" ] ||  [ ! -x "$(command -v xsel)" ] || [ ! -x "$(command -v proxychains)" ] \
-    [ ! -x "$(command -v yad)" ] || [[ ! -f $HOME/.local/share/applications/Zad-dl.desktop ]] \
-   || [[ ! -d $HOME/.zad-dl ]] || [[ ! -f $HOME/.zad-dl/gtao4rj.png ]] \
-   || [[ ! -f $HOME/.zad-dl/Zad-dl.sh ]]; then
+	   [ ! -x "$(command -v yad)" ] || [[ ! -f $HOME/.local/share/applications/Zad-dl.desktop ]] \
+	   || [[ ! -d $HOME/.zad-dl ]] || [[ ! -f $HOME/.zad-dl/gtao4rj.png ]] \
+	   || [[ ! -f $HOME/.zad-dl/Zad-dl.sh ]]; then
+	   echo -e "#If this is your first time using zad-dl it will automatically check and install programms and packages required for running this app.\n\nPlease provide necessary password when asked.\n\nIt may take few minutes to download all packages"
+	   if ! [ -x "$(command -v youtube-dl)" ]; then
+	   echo -e "\nInstalling youtube-dl\n"
+	   sudo add-apt-repository ppa:nilarimogard/webupd8 
+	   sudo apt-get update 
+	   sudo apt-get install youtube-dl
+	   fi
+	   if ! [ -x "$(command -v xsel)" ]; then
+	   echo -e "\nInstalling xsel\n"
+	   sudo apt-get install xsel 
+	   fi
+	   if ! [ -x "$(command -v proxychains)" ]; then
+	   echo -e "\nInstalling proxychains\n"
+	   sudo apt-get install proxychains tor obfsproxy
+	   fi
+	   if ! [ -x "$(command -v yad)" ]; then
+	   echo -e "\nInstalling yad\n"
+	   sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
+	   sudo apt-get update
+	   sudo apt-get install yad
+	   fi
 
-echo -e "#If this is your first time using zad-dl it will automatically check and install programms and packages required for running this app.\n\nPlease provide necessary password when asked.\n\nIt may take few minutes to download all packages"
-
-
-if ! [ -x "$(command -v youtube-dl)" ]; then
-echo -e "\nInstalling youtube-dl\n"
-sudo add-apt-repository ppa:nilarimogard/webupd8 
-sudo apt-get update 
-sudo apt-get install youtube-dl
-fi
-if ! [ -x "$(command -v xsel)" ]; then
-echo -e "\nInstalling xsel\n"
-sudo apt-get install xsel 
-fi
-if ! [ -x "$(command -v proxychains)" ]; then
-echo -e "\nInstalling proxychains\n"
-sudo apt-get install proxychains tor obfsproxy
-fi
-if ! [ -x "$(command -v yad)" ]; then
-echo -e "\nInstalling yad\n"
-sudo add-apt-repository ppa:webupd8team/y-ppa-manager 
-sudo apt-get update
-sudo apt-get install yad
-fi
 if [[ ! -f $HOME/.local/share/applications/Zad-dl.desktop ]]; then
 echo -e "\nCreating a Launcher file details and icon for current user\n"
 cat >$HOME/.local/share/applications/Zad-dl.desktop<<EOL
@@ -151,7 +137,6 @@ Type=Application
 StartupNotify=true
 EOL
 fi
-
 if [[ ! -d $HOME/.zad-dl ]] || [[ ! -f $HOME/.zad-dl/gtao4rj.png ]] || [[ ! -f $HOME/.zad-dl/Zad-dl.sh ]]; then
 echo -e "\nDownloading Icon for Launcher\n"
 mkdir -p $HOME/.zad-dl/
@@ -159,10 +144,7 @@ cp $(readlink -f $0) $HOME/.zad-dl/Zad-dl.sh
 wget -N -P $HOME/.zad-dl/ http://i.imgur.com/gtao4rj.png
 #icon courtsey https://www.iconfinder.com/icons/406708/download_icon#size=128
 fi
-
 else
 echo -e "\nAll programms and packages installed"
-
 YouDown;
 fi
-
